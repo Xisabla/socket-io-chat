0f4a10a6f981e39c188ae3fba48df03ad4993248
Gautier Miquet <xisabla.dev@gmail.com>
2017-08-29 00:48:09 +0200

    Global users features

--- index.js
+++ index.js
@@ -7,14 +7,20 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 
-// Logger
+// Lib requires
 var Logger = require('./lib/logger');
+var User = require('./lib/user');
+
+// Objects init
 var Console = new Logger();
 
 // Setting up express server with Socket.IO
 var app = express();
 var server = http.createServer(app);
 var io = require('socket.io')(server);
+
+var users = [];
+var channels = [];
 
 // Server config
 app.set('port', (process.env.PORT || 5000));
@@ -28,8 +34,30 @@ app.use('/', require('./routes/index'));
 
 // Socket.IO
 io.on('connection', function (socket) {
-    socket.emit('welcome', 'Hello World');
+    // Send users-list
+    socket.emit('users-list', users); // Send users list
     Console.log('Socket.IO', 'New client connected', chalk.cyan);
+
+    // Register new user & update users list
+    socket.on('new-user', function (username) {
+        var user = new User(username);
+        users.push(user);
+        socket.user = user;
+        socket.emit('session', user.getSession());
+        socket.emit('channels-list', channels);
+        io.emit('users-list', users);
+        Console.log('Socket.IO', 'New user joined: ' + username, chalk.yellow);
+        Console.debug('User list updated: ' + users);
+    });
+
+    socket.on('disconnect', function () {
+        if (socket.user) {
+            var index = users.indexOf(socket.user);
+            Console.log('Socket.IO', users[index]['username'] + ' disconnected', chalk.red);
+            users.splice(index, 1);
+            io.emit('users-list', users);
+        }
+    });
 });
 
 // Starting server
--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,59 @@
-/*$(document).ready(function () {
-    resizeChatClient();
+var data = {};
 
-    $(window).resize(resizeChatClient);
+$(document).ready(function () {
+
+    var socket = io();
+    var data = getData();
+
+    socket.on('users-list', function (users) {
+        data.users = users;
+        $('#chat-users-list').text('');
+        users.forEach(function (user) {
+            $('#chat-users-list').append($('<li>').html('<a href="#">' + user.username + '</a>'));
+        });
+    });
+
+    socket.on('session', function (session) {
+        data.session = session;
+        $('#chat-username').modal('hide');
+        $('#chat-client').append($('<li>').addClass('message').html('Welcome <strong>' + data.username + '</strong><br>Join a channel now or create one 😏'));
+        $('#chat-client-form input[name=message]').focus();
+    });
+
+    if (!data.session) {
+        $('#chat-username').modal({
+            backdrop: 'static',
+            keyboard: false
+        });
+
+        setTimeout(function () {
+            $('#chat-username-form input[name=username]').focus();
+        }, 600);
+
+        $('#chat-username-form').submit(function () {
+            var valid = true;
+            var username = $('#chat-username-form input[name=username]').val();
+
+            data.users.forEach(function (user) {
+                if (user.username == username) {
+                    valid = false;
+                }
+            });
+
+            if (valid) {
+                data.username = username;
+                $('#chat-username-ok').show(500);
+                $('#chat-username-form input[name=username]').addClass('disabled').prop('disabled', true);
+                $('#chat-username-form button[type=submit]').addClass('disabled').prop('disabled', true);
+                socket.emit('new-user', username);
+            } else {
+                $('#chat-username-taken').show(500);
+            }
+            return false;
+        });
+    }
 });
 
-function resizeChatClient() {
-    $('#chat-client').css('height', $('body').height() - $('#chat-navigation').height() - 1);
-}*/
+function getData() {
+    return {};
+}
