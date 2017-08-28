e5b8e8c492aafb35bbed30defc75b31fa350a55b
Gautier Miquet <xisabla.dev@gmail.com>
2017-08-28 19:42:29 +0200

    Main interface style & guidelines

---
+++ public/js/app.js
@@ -0,0 +1,9 @@
+/*$(document).ready(function () {
+    resizeChatClient();
+
+    $(window).resize(resizeChatClient);
+});
+
+function resizeChatClient() {
+    $('#chat-client').css('height', $('body').height() - $('#chat-navigation').height() - 1);
+}*/
